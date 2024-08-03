2ff60224c070551f0859bde881d59e8a577967e5
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-03 21:46:20 +0900

    char v3

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,4 +11,43 @@ socket.emit('join', { username, room }, (error) => {
   }
 });
 
-socket.on('message')
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, { room, users });
+  document.querySelector('#sidebar').innerHTML = html;
+})
+
+const messages = document.querySelector('#messages');
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+socket.on('message', ({username, text, createdAt}) => {
+  const html = Mustache.render(messageTemplate, {username, text, createdAt: moment(createdAt).format('h:mm a')});
+  messages.insertAdjacentHTML('beforeend', html);
+  scrollToBottom();
+})
+
+function scrollToBottom() {
+  const scrollHeight = messages.scrollHeight;
+  const height = messages.clientHeight;
+  messages.scrollTop = scrollHeight - height;
+}
+
+const messageForm = document.querySelector('#message-form');
+const messageFormInput = document.querySelector('input');
+const messageFormButton = document.querySelector('button');
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  messageFormButton.setAttribute('disabled', 'disabled');
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message, (error) => {
+    messageFormButton.removeAttribute('disabled');
+    messageFormInput.value = '';
+    messageFormInput.focus();
+
+    if(error) {
+      return console.log(error);
+    }
+  })
+})
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const http = require('http');
 const { Server } = require('socket.io');
 
-const { addUser, getUsersInRoom } = require('./utils/users');
+const { addUser, getUsersInRoom, getUser } = require('./utils/users');
 const { generateMessage } = require('./utils/messages');
 
 const app = express();
@@ -36,7 +36,12 @@ io.on('connection', (socket) => {
       });
   });
 
-  socket.on('sendMessage', () => {});
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    
+    io.to(user.room).emit('message', generateMessage(user.username, message));
+    callback();
+  });
 
   socket.on('disconnect', () => {
     console.log('socket disconnected', socket.id);
